762a64b772f217e28e3c3f503912f1902d826514
ptarou <62635668+uemagi@users.noreply.github.com>
2021-05-25 12:51:01 +0900

    Add files via upload

--- app7.py
+++ app7.py
@@ -3,7 +3,6 @@ from google.cloud import texttospeech
 
 import io 
 import streamlit as st
-from streamlit.elements.color_picker import ColorPickerMixin
 
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'secret.json'
@@ -82,7 +81,7 @@ if input_data is not None:
         ('default','male','female','neutral')
     )
     st.markdown('### 音声合成')
-    st.write('しゃーない読んだるから耳の穴かっぽじってよー聞けよ？ほなここ押してみ！！')
+    st.write('この文読んだるからちゃーんと聞いとけよ？')
     if st.button('しゃーない読んだるから耳の穴かっぽじってよー聞けよ？ほなここ押してみ！！'):
         comment = st.empty()
         comment.write('音声出力を開始するで？ええな？')

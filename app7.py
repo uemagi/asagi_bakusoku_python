895b072a7be03088177e5283009b0e7c922c4ee2
ptarou <62635668+uemagi@users.noreply.github.com>
2021-05-25 12:50:03 +0900

    Add files via upload

--- app7.py
+++ app7.py
@@ -82,7 +82,7 @@ if input_data is not None:
         ('default','male','female','neutral')
     )
     st.markdown('### 音声合成')
-    st.write('しゃーない読んだるから耳の穴かっぽじってよー聞けよ？ほなここ押してみ！！',  Color= 'orange')
+    st.write('しゃーない読んだるから耳の穴かっぽじってよー聞けよ？ほなここ押してみ！！')
     if st.button('しゃーない読んだるから耳の穴かっぽじってよー聞けよ？ほなここ押してみ！！'):
         comment = st.empty()
         comment.write('音声出力を開始するで？ええな？')
